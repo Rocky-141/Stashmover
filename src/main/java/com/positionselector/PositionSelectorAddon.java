b703c14d301526483defc30262dae84482b74f0b
rocky_141 <sdavis2023@stmc.ie>
2026-05-04 22:47:07 +0100

    Add AutoMacro module to PositionSelectorAddon
    updated file

--- src/main/java/com/positionselector/PositionSelectorAddon.java
+++ src/main/java/com/positionselector/PositionSelectorAddon.java
@@ -1,18 +1,16 @@
 package com.positionselector;
 
-
 import com.positionselector.module.AutoChestSteal;
 import com.positionselector.module.AutoStashSteal;
 import com.positionselector.module.AutoChestDeposit;
 import com.positionselector.module.PearlLoaderModule;
 import com.positionselector.module.AutoStashDeposit;
 import com.positionselector.module.AutoStashCycle;
-
+import com.positionselector.module.AutoMacro;
 
 import meteordevelopment.meteorclient.addons.MeteorAddon;
 import meteordevelopment.meteorclient.systems.modules.Modules;
 import meteordevelopment.meteorclient.commands.Commands;
-
 
 import com.positionselector.commands.PositionCenterCommand;
 import com.positionselector.commands.PositionLookCommand;
@@ -22,14 +20,17 @@ public class PositionSelectorAddon extends MeteorAddon {
 
     @Override
     public void onInitialize() {
-        
+
+        // Register modules
         Modules.get().add(new AutoChestSteal());
         Modules.get().add(new AutoChestDeposit());
         Modules.get().add(new PearlLoaderModule());
         Modules.get().add(new AutoStashSteal());
         Modules.get().add(new AutoStashDeposit());
         Modules.get().add(new AutoStashCycle());
+        Modules.get().add(new AutoMacro());
 
+        // Register commands
         Commands.add(new PositionCenterCommand());
         Commands.add(new PositionLookCommand());
         Commands.add(new ItemNameCommand());
